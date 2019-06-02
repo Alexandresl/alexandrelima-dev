4a1e39b5a7ee21a12b74538c90b4829a4dc045a7
Alexandre Lima <limaalexguitar@gmail.com>
2019-06-02 18:46:00 -0300

    go-down

--- cmb/script/script.js
+++ cmb/script/script.js
@@ -12,12 +12,20 @@ $('a').click(function() {
 function scrollBanner() {
     var scrollPos;
     var headerText = document.querySelector('#header-content');
+    var bola = document.querySelector('.go-down img')
+    var seta = document.querySelector('.seta-go-down');
     scrollPos = window.scrollY;
   
     if (scrollPos <= 600) {
         headerText.style.transform =  "translateY(" + (-scrollPos/3) +"px" + ")";
         headerText.style.opacity = 1 - (scrollPos/600);
+        bola.style.transform =  "translateY(" + (-scrollPos/1.2) +"px" + ")";
+        bola.style.opacity = 1 - (scrollPos/600);
+        seta.style.transform =  "translateY(" + (-scrollPos/1.2) +"px" + ")";
+        seta.style.opacity = 1 - (scrollPos/600);
     }
   }
   
   window.addEventListener('scroll', scrollBanner);
+
+  // Bola pulando
