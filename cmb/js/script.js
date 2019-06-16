aa4bb003e4fa8246d1992f2ef84b8b96a1e789e9
Alexandre Lima <limaalexguitar@gmail.com>
2019-06-16 16:52:42 -0300

    160620191652

--- cmb/js/script.js
+++ cmb/js/script.js
@@ -28,17 +28,13 @@ $('.menu-item > a').click(function () {
 function scrollBanner() {
     var scrollPos;
     var headerText = document.querySelector('#header-content');
-    var bola = document.querySelector('.go-down img')
-    var seta = document.querySelector('.seta-go-down');
+    
     scrollPos = window.scrollY;
 
     if (scrollPos <= 600) {
         headerText.style.transform = "translateY(" + (-scrollPos / 3) + "px" + ")";
         headerText.style.opacity = 1 - (scrollPos / 600);
-        bola.style.transform = "translateY(" + (-scrollPos / 1.2) + "px" + ")";
-        bola.style.opacity = 1 - (scrollPos / 600);
-        seta.style.transform = "translateY(" + (-scrollPos / 1.2) + "px" + ")";
-        seta.style.opacity = 1 - (scrollPos / 600);
+        
     }
 }
 
@@ -52,4 +48,9 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('select').material_select();
 });
+
+// inicializa modais
+$(document).ready(function(){
+    $('.modal').modal();
+  });
 
